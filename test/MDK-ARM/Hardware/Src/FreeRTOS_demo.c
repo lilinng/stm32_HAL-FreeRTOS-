adef01605172def74f7c70e94a20aa4695e9a69a
lilinng <2464532129@qq.com>
2026-02-20 21:01:19 +0800

    事件标志组的应用

--- test/MDK-ARM/Hardware/Src/FreeRTOS_demo.c
+++ test/MDK-ARM/Hardware/Src/FreeRTOS_demo.c
@@ -1,7 +1,7 @@
 /*
  * @Author: lilinng 2464532129@qq.com
  * @Date: 2026-02-12 18:52:37
- * @LastEditTime: 2026-02-19 15:01:36
+ * @LastEditTime: 2026-02-20 20:59:06
  * @FilePath: \test_EIDEd:\MCU\stm32\stm32_practise\VS+HAL\stm32_hd_c\test\MDK-ARM\Hardware\Src\FreeRTOS_demo.c
  * @Description: 用于练习FreeRTOSapi
  */
@@ -9,6 +9,7 @@
 #include "task.h"   //任务相关头文件,和FreeRTOS.h一样为必须包含的头文件
 #include "queue.h"  //队列相关头文件
 #include "semphr.h" //信号量相关头文件
+#include "event_groups.h"   //事件标志组头文件
 #include "stm32f1xx_hal.h"
 #include "stdio.h"
 #include "gpio.h"
@@ -33,11 +34,12 @@ TaskHandle_t task2_handle;
 #define START_TASK3_PRIORITY 4
 TaskHandle_t task3_handle;
 
-//信号量句柄,也是队列句柄
-QueueHandle_t   queue1_handle = NULL;
-QueueHandle_t   binary_handle = NULL;
-QueueHandle_t   User_handle;
+//事件标志组句柄
+EventGroupHandle_t  User_handle;
 
+//事件标志组相关定义
+#define EVENT_GROUP_BIT_0   (1<<0)
+#define EVENT_GROUP_BIT_1   (1<<1)
 /**
  * @description: 启动任务，用于创建其他进程并在创建完成后删除自身
  * @param {void} *pvParameters
@@ -62,41 +64,19 @@ void task3(void *pvParameters);
  */
 void FreeRTOS_Start(void)
 {
-    //创建一个队列一个信号量
-    queue1_handle = xQueueCreate(2,sizeof(KEY_ENUM));
-    //默认不释放信号量
-    binary_handle = xSemaphoreCreateBinary();
-    if(queue1_handle != NULL && binary_handle != NULL)
+    //事件标志组创建
+    /**
+     * @description: 创建事件标志组
+     * @return: {EventGroupHandle_t} 返回句柄
+     */
+    User_handle = xEventGroupCreate();
+    if(User_handle != NULL)
     {
-        printf("queue1 & binary create successfully\r\n");
+        printf("event group create successfully\r\n");
     }
     else
     {
         printf("create failed\r\n");
-    }
-    //创建队列集
-    /**
-     * @description: 参数为传入的队列个数
-     * @return {QueueSetHandle_t}
-     */
-    User_handle = xQueueCreateSet(2);
-    if(User_handle != NULL)
-    {
-        printf("queue set create successfully\r\n");
-    }
-    //将队列添加到队列集
-    /**
-     * @description: 第一个参数为需要添加的队列，第二个为目标队列集
-     * @return {BaseType_t}
-     */
-    BaseType_t res1 = 0;
-    BaseType_t res2 = 0;
-    //添加之前队列必须为空
-    res1 = xQueueAddToSet(queue1_handle,User_handle);  
-    res2 = xQueueAddToSet(binary_handle,User_handle);
-    if(res1 == pdPASS && res2 == pdPASS)
-    {
-        printf("add successfully\r\n");
     }
     //创建一个启动任务
     xTaskCreate((TaskFunction_t)start_task,
@@ -144,88 +124,59 @@ void start_task(void *pvParameters)
     vTaskDelete(NULL);
 }
 /**
- * @description: 用于按键扫描，当key1按下，往队列写入数据；当key2按下，释放二值信号量
- * @param {void} *pvParameters
+ * @description: 读取按键按下键值，根据不同键值将事件标志组相应事件置1，模拟事件发生
  * @return {*}
  */
 void task1(void *pvParameters)
 {
-    BaseType_t res;
     KEY_ENUM key_value = NO_PRESS;
     while (1)
     {
         key_value = Key_Scan();
         if(key_value == KEY1)
         {
-            res = xQueueSend(queue1_handle,&key_value,portMAX_DELAY);
-            if(res == pdPASS)
-            {
-                printf("send successfully\r\n");
-            }
+            //key1按下，对bit0置1
+            /**
+             * @description: 第一个参数句柄，第二个参数置?
+             * @return {EventBits_t}
+             */
+            xEventGroupSetBits(User_handle,EVENT_GROUP_BIT_0);
+            printf("key1 pressed\r\n");
         }
         else if(key_value == KEY2)
         {
-            res = xSemaphoreGive(binary_handle);
-            if(res == pdPASS)
-            {
-                printf("give successfully\r\n");
-            }
-            else
-            {
-                printf("give failed\r\n");
-            }
+            //key2按下，对bit1置1
+            xEventGroupSetBits(User_handle,EVENT_GROUP_BIT_1);
+            printf("key2 pressed\r\n");
         }
-        vTaskDelay(1000);
+        vTaskDelay(500);
     }
 }
 /**
- * @description: 读取队列集中的消息并输出
+ * @description: 同时等待事件标志组中的多个事件位，当这些事件位都置一的话执行相应的处理
  * @param {void} *pvParameters
  * @return {*}
  */
 void task2(void *pvParameters)
 {
-    QueueSetMemberHandle_t member_handle;
-    uint8_t receive;
-    BaseType_t res;
+    EventBits_t res;
     while (1)
-    {
-       /**
-        * @description: 查看哪个队列有数据来了;如果是多个队列数据同时就绪，多次调用得到每个就绪队列的句柄
-        * @return {QueueSetMemberHandle_t}
-        */
-       member_handle = xQueueSelectFromSet(User_handle,portMAX_DELAY);
-       //根据队列去获取数据
-       if(member_handle == queue1_handle)
-       {
-            //读取队列数据
-            res = xQueueReceive(queue1_handle,&receive,portMAX_DELAY);
-            if(res == pdPASS)
-            {
-                printf("take queue1:%d\r\n",receive);
-            }
-            else
-            {
-                printf("take failed\r\n");
-            }
-       }
-       else if(member_handle == binary_handle)
-       {
-            //获取信号量
-            res = xSemaphoreTake(binary_handle,portMAX_DELAY);
-            if(res == pdPASS)
-            {
-                printf("get binary successfully\r\n");
-            }
-            else
-            {
-                printf("get faild\r\n");
-            }
-       }
-       else
-       {
-            printf("queue set return is NULL\r\n");
-       }
+    {       
+        /**
+         * @description: 
+         * @param {EventGroupHandle_t} xEventGroup  句柄
+         * @param {EventBits_t} uxBitsToWaitFor     需要等待的标志位
+         * @param {BaseType_t} xClearOnExit         退出时是否清零所有标志位
+         * @param {BaseType_t} xWaitForAllBits      是否等待所有标志位成立
+         * @param {TickType_t} xTicksToWait         等待期间进入阻塞状态
+         * @return {EventBits_t}
+         */
+        res = xEventGroupWaitBits(User_handle,
+                            EVENT_GROUP_BIT_0|EVENT_GROUP_BIT_1,
+                            pdTRUE,     //pdFALSE
+                            pdTRUE,     //pdFALSE
+                            portMAX_DELAY);
+        printf("task2 event group:%#x\r\n",res);
     }
 }
 void task3(void *pvParameters)
@@ -233,33 +184,7 @@ void task3(void *pvParameters)
     BaseType_t res;
     while(1)
     {
-        //获取并输出信号量
-        res = xSemaphoreTake(User_handle,portMAX_DELAY);
-        if(res == pdPASS)
-        {
-            printf("High priority task semaphor successfully\r\n");
-        }
-        else
-        {
-            printf("High priority task semaphor failed\r\n");
 
-        }
-        //执行其他逻辑(延时)
-        printf("task 3 running\r\n");
-        HAL_Delay(3000);
-
-        res = 0;
-        
-        //释放信号量
-        res = xSemaphoreGive(User_handle);
-        if(res == pdPASS)
-        {
-            printf("task 3 Give successfully\r\n");
-        }
-        else 
-        {
-            printf("task 3 Give failed\r\n");
-        }
         vTaskDelay(1000);
     }
 }
