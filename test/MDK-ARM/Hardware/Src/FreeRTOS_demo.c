9b88442ca3443af51e961020ef774a52f80877dc
lilinng <2464532129@qq.com>
2026-02-23 14:43:05 +0800

    完善注释

--- test/MDK-ARM/Hardware/Src/FreeRTOS_demo.c
+++ test/MDK-ARM/Hardware/Src/FreeRTOS_demo.c
@@ -1,3 +1,11 @@
+/*
+ * @Author: lilinng 2464532129@qq.com
+ * @Date: 2026-02-23 14:34:32
+ * @LastEditTime: 2026-02-23 14:42:56
+ * @FilePath: \test_EIDEd:\MCU\stm32\stm32_practise\VS+HAL\stm32_hd_c\test\MDK-ARM\Hardware\Src\FreeRTOS_demo.c
+ * @Description: 
+ */
+
 #include "FreeRTOS.h"
 #include "task.h"
 #include "stm32f1xx_hal.h"
